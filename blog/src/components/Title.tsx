1ffe832ec3aca1ed6294b9ea667ac4bbe2b3279a
LEE DONG GYU <tenenger7125@gmail.com>
2023-07-10 16:01:56 +0900

    fix: fix title line

--- blog/src/components/Title.tsx
+++ blog/src/components/Title.tsx
@@ -5,27 +5,30 @@ type TitleProps = {
   children: React.ReactNode | string;
   line?: number;
   $line?: number;
+  fz?: number;
+  $fz?: number;
 };
 
 const STitle = styled.div<Partial<TitleProps>>`
   margin: 10px 0;
+  font-size: ${(props) => props.$fz && props.$fz}px;
   font-weight: 700;
 
   ${(props) =>
-    props.line &&
+    props.$line &&
     `
     display: -webkit-box;
     -webkit-box-orient: vertical;
-    -webkit-line-clamp: ${props.line};
+    -webkit-line-clamp: ${props.$line};
     overflow: hidden;
     text-overflow: ellipsis;
     `}
 `;
 
-const Title = ({ order, line, children }: TitleProps) => {
+const Title = ({ order, line, fz, children }: TitleProps) => {
   const TitleOrder: keyof JSX.IntrinsicElements = `h${order}`;
   return (
-    <STitle as={TitleOrder} $line={line}>
+    <STitle as={TitleOrder} $line={line} $fz={fz}>
       {children}
     </STitle>
   );
