9c5cecf751f85cc47699483a819d3b7909de751d
LEE DONG GYU <tenenger7125@gmail.com>
2023-07-10 16:21:43 +0900

    fix: rename layout component folder name

---
+++ blog/src/components/Layout/index.tsx
@@ -0,0 +1,18 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
